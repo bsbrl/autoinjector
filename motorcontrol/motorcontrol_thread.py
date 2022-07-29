95c94f21993e989f547695db303e8d5f3c603637
Jacob O'Brien <obria006@umn.edu>
2022-07-29 17:50:57 +0200

    Improved injection trajectory for 4-axis sensapex

--- motorcontrol/motorcontrol_thread.py
+++ motorcontrol/motorcontrol_thread.py
@@ -23,11 +23,10 @@ class motorcontroller(QThread):
         QThread.__init__(self)
         self.ump = UMP.get_ump()
         self.devids = self.ump.list_devices()
-        self.devs = {i:SensapexDevice(i)
-         for i in self.devids}
-        self.zdrift = zdrift
-        self.stepsizex = stepsizex
-        self.stepsizey = stepsizey
+        self.devs = {i:SensapexDevice(i) for i in self.devids}
+        self.zdrift = zdrift # copmuted as delta x for
+        self.stepsizex = stepsizex # computed as delta d for given pipette angle
+        self.stepsizey = stepsizey # delta y from last positoin to this positoin
         self.approachdist = approachdist
         self.injectiondepth = injectiondepth
         self.speed = speed
@@ -48,13 +47,13 @@ class motorcontroller(QThread):
             time.sleep(0.3)
             position0 = self.devs[1].get_pos() #the delay should fix it        
 
-        # if using a 3-axis manipulator, the xaxis is 0. Else, it's 3
+        # x axis is 0. d axis is 3 (if device has it)
         if len(position0) == 3:
             xaxis = 0
         elif len(position0) == 4:
             xaxis = 3
 
-        #go to edge of tissue 
+        #go to edge of tissue (by advance x or d axis from current positoin by computed value of delta d)
         position1 = position0[:]
         position2 = position1[:]
         position2[xaxis] += (self.stepsizex)
@@ -62,7 +61,7 @@ class motorcontroller(QThread):
         print("edge of tissue" + str(position2))
         time.sleep(0.25)
 
-        #go into tissue and inject
+        #go into tissue and inject (by advance x or d axis from edge positoin by passed depth value)
         pos2 = position2
         position3 = position2[:]
         position3[xaxis] += self.injectiondepth
@@ -71,28 +70,28 @@ class motorcontroller(QThread):
         print("injection site" + str(position3))
         time.sleep(0.25)
 
-        #pull out of tissue
+        #pull out of tissue (by retract x or d axis from inj positoin by depth and approach dist)
         posfinalout = position3[:]
         posfinalout[xaxis]-= (self.injectiondepth+self.approachdist)
         self.devs[1].goto_pos(posfinalout, self.speed)
         print("pull out tissue position = " + str(posfinalout))
         time.sleep(0.25)
         
-        #step along tissue in y direction
+        #step along tissue in y direction (by move y axis from retract position by y step)
         positiony = posfinalout[:]
         positiony[1] += self.stepsizey
         self.devs[1].goto_pos(positiony, self.speed)
         print("move in y pos = " + str(positiony))
         time.sleep(0.25)
 
-        #correct for Zdrift if applicable
+        # change z position (by move z axis from new y positoin by computed value of delta x)
         positionzdrift = positiony[:]
         positionzdrift[2] += self.zdrift
         self.devs[1].goto_pos(positionzdrift, self.speed)
         print("z drift position = " + str(positionzdrift))
         time.sleep(0.25)
 
-        #go to edge of tissue
+        #go to edge of tissue (by move x or d axis by passed approach distance)
         positionedge = positionzdrift[:]
         positionedge[xaxis] += self.approachdist
         self.devs[1].goto_pos(positionedge, self.speed)
@@ -128,6 +127,194 @@ class motorcontroller(QThread):
         self.devs[1].goto_pos(end_pos, 1000)
 
 
+class motorcontroller_improved(QThread):
+    """
+    QThread class to control 4-axis sensapex manipulators and perform a
+    prototypical injection into the tissue. This was designed for a 4-axis
+    manipulator but includes code for 3-axis, but it is untested on a 3-axis
+    manipualtor.
+    
+    This class assumes you want to move from a micropipette tip located on the
+    edge of a tissue to perform injections at the next location. There are 4
+    stereo typed positions along an injection trajectory
+        1. Preinjection position:
+                pipette injection-axis (x-axis for 3 axes manipulators and
+                d-axis for 4 axes manipulators) aligned with the injection
+                position with the tip located "approach distance" away from the
+                tissue edge (along the injection-axis).
+        2. Injection position:
+                pipette tip in focus at the desired injection position. This
+                position is acheived by advancing the injection-axis by
+                sum("approach distance" + "injection distance")
+        3. Retracted position:
+                pipette tip is out-of-focus at the edge of the tissue. This
+                position is acheived by retracting the injection axis by the
+                "injection distance"
+        4. Pullout positon:
+                pipette tip further retracts from the edge of the tissue by
+                retracting injection axis by "approach distance". THis is so
+                the pipette doesn't collide with the tissue when displacing
+                laterally to the next preinjection positoin
+
+    This class is designed to move between "retracted positions" corresponding
+    to different injection coordinates.
+    
+    Arguments:
+        inj_ind: Index of injection (starting at 1)
+        dx: (nm) required manipulator displacement along a "pure" x-axis to get
+            from the current "retracted position" to the next "retracted
+            position". "Pure" meaning that the displacement occurs in the image
+            plane (not a mixture of moving in the image plane and along the focal
+            axis).
+        dy: (nm) required manipulator displacement along its y-axis to get from
+            the current "retracted position" to the next "retracted position".
+        dx_rate: Amount of "pure x-axis" displacement that occurs for a
+            1 unit move along the injection axis.
+        dz_rate: Amount of z-axis/focal-axis displacement that occurs for a
+            1 unit move along the injection axis.
+        approachdist: (nm) Distance from which to start approaching tissue for
+            injection.
+        injectiondepth: (nm) Distance to injecte below annotated tissue surface
+        speed: (um/s) Speed of injections
+    """
+
+    signal = pyqtSignal(str)
+
+    def __init__(self, inj_ind, dx, dy, dx_rate, dz_rate, approachdist, injectiondepth, speed):
+        QThread.__init__(self)
+        self.ump = UMP.get_ump()
+        self.devids = self.ump.list_devices()
+        self.devs = {i:SensapexDevice(i) for i in self.devids}
+        self.inj_ind = inj_ind
+        self.stepsizex = dx
+        self.stepsizey = dy
+        self.dx_rate = dx_rate
+        self.dz_rate = dz_rate
+        self.approachdist = approachdist
+        self.injectiondepth = injectiondepth
+        self.speed = speed
+        if len(self.devs[1].get_pos()) == 3:
+            self.n_axes = 3
+            self.axes = {'x':0,'y':1,'z':2,'d':0}
+        elif len(self.devs[1].get_pos()) == 4:
+            self.n_axes = 4
+            self.axes = {'x':0,'y':1,'z':2,'d':3}
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        self.run_trajectory()
+
+    def run_trajectory(self):
+        '''
+        Trajectory assumes your starting at a retraction position. Trajectory
+        path:
+
+        1. If you're doing the first injection (tip in inj-plane not at tissue)
+                a. Move z axis away from tissue by inj. depth projected on z
+                   axis
+        2. Go to "Pullout position" by retracting injection axis by the
+            "approach distance".
+        3. Go to "Preinjection position" by displacing x-,y- axes by the
+            specified/computed displacements.
+        4. Go to "Injection position" by advancing injection axis by the
+            sum("approach distance" + "injection depth")
+        5. Go to "Retraction position" by retracing injection axis by
+            "injection depth".
+        6. Repeat 1-6
+        7. If you're not doing any more injections
+                a. Move z-axis towards from tissue by inj. depth projected on z
+                   axis (to bring back in focus).
+                b. Retract x-axis by 40 um to move away from tissue.
+        '''
+        position0 = self.devs[1].get_pos() #current position
+        print("position0 = " +str(position0))
+        
+        #sometimes there is an error with the sensapex manipulator and it says its position is zero, this ruins the trajectory
+        if position0[0] < 100:
+            print('sensapex error, wait')
+            time.sleep(0.3)
+            position0 = self.devs[1].get_pos() #the delay should fix it
+        
+        # If first injection, then displace z axis so the injection positiongs are in focus
+        start_pos = position0[:]
+        if self.inj_ind == 1:
+            start_pos[self.axes['z']] -= int(self.injectiondepth*self.dz_rate)
+            move_req=self.devs[1].goto_pos(start_pos, self.speed)
+            print(f'Starting position:\t{start_pos}',end='... ')
+            while move_req.finished is False:
+                time.sleep(0.1)
+            print('position reached')
+        
+        # Retract injection axis (by approach distance)
+        pullout_pos = start_pos[:]
+        pullout_pos[self.axes['d']] -= int(self.approachdist)
+        move_req = self.devs[1].goto_pos(pullout_pos, self.speed)
+        print(f'Pull out position:\t{pullout_pos}',end='... ')
+        while move_req.finished is False:
+            time.sleep(0.1)
+        print('position reached')
+
+        # Move laterally along x and y to align with next injection postion
+        preinject_pos = pullout_pos[:]
+        preinject_pos[self.axes['y']] += int(self.stepsizey)
+        # When moving x-axis for 3axis manip your actually moving "down" and "forward"
+        if self.n_axes == 3:
+            delta_inj_axis = self.stepsizex / self.dx_rate
+            preinject_pos[self.axes['x']] += int(delta_inj_axis)
+            preinject_pos[self.axes['z']] -= int(self.dz_rate*delta_inj_axis)
+        elif self.n_axes == 4:
+            preinject_pos[self.axes['x']] += int(self.stepsizex)
+        move_req = self.devs[1].goto_pos(preinject_pos, self.speed)
+        print(f'Preinject position:\t{preinject_pos}',end='... ')
+        while move_req.finished is False:
+            time.sleep(0.1)
+        print('position reached')
+
+        # go into tissue and inject
+        inject_pos = preinject_pos[:]
+        inject_pos[self.axes['d']] += int((self.injectiondepth + self.approachdist))
+        move_req = self.devs[1].goto_pos(inject_pos, self.speed)
+        print(f'Injection position:\t{inject_pos}',end='... ')
+        while move_req.finished is False:
+            time.sleep(0.1)
+        print('position reached')
+        time.sleep(0.1)
+
+        # retract to tissue edge
+        retract_pos = inject_pos[:]
+        retract_pos[self.axes['d']] -= int(self.injectiondepth)
+        move_req = self.devs[1].goto_pos(retract_pos, self.speed)
+        print(f'Retraction position:\t{retract_pos}',end='... ')
+        while move_req.finished is False:
+            time.sleep(0.01)
+        print('position reached')
+               
+
+    def finalpullout(self, dist, zdist):
+        current_pos = self.devs[1].get_pos()
+
+        #in some cases, sensapex does not report a correct xyz pos, thus we wait
+        while len(current_pos) < 3:
+            current_pos = self.devs[1].get_pos()
+            time.sleep(0.01)      
+            print("current pos =" + str(current_pos))
+
+        #in some cases, sensapex says xaxis = 2, but this is just a bug so we wait
+        while current_pos[self.axes['x']] == 2:
+            current_pos = self.devs[1].get_pos()
+            time.sleep(0.01)
+            print("error, new current pos =" + str(current_pos))
+
+        # Displace tissue away from edge and down into focal plane
+        end_pos = current_pos[:]
+        end_pos[self.axes['x']] -= dist
+        end_pos[self.axes['z']] += self.injectiondepth*self.dz_rate
+        if self.n_axes == 3:
+            end_pos[2] += dist*self.dz_rate
+        print("end pos =" + str(end_pos))
+        self.devs[1].goto_pos(end_pos, 1000)
 
 
 #---------------------Thread Class for motor position----------------------------
--- motorcontrol/trajectorythread_minimal.py
+++ motorcontrol/trajectorythread_minimal.py
@@ -2,8 +2,9 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 import os, sys, time
 from .motorcontrol_thread import motorcontroller as mc
+from .motorcontrol_thread import motorcontroller_improved as mci
 import numpy as np
-
+import traceback
 
 
 class trajectoryimplementor(QThread):
@@ -28,15 +29,15 @@ class trajectoryimplementor(QThread):
     def __init__(self, xytheta, thetaz, currentpos, pixelsize, approachdist, depthintissue, spacingbtwn, edgecoord, ncells, speed):
         QThread.__init__(self)
         self.stopsignal = False
-        self.xthetarad = xytheta
-        self.thetaz = thetaz
-        self.currentposition = currentpos
-        self.pixelsize = pixelsize
-        self.approachdist = approachdist
-        self.depthintissue = depthintissue
+        self.xthetarad = xytheta # angle between vertical and man y or horz and man x
+        self.thetaz = thetaz # pipette angle
+        self.currentposition = currentpos # Pixel coordinates of clicked tip
+        self.pixelsize = pixelsize # um per pixel
+        self.approachdist = approachdist # nm of approach dist
+        self.depthintissue = depthintissue # nm of depth in tissue
         self.spacingbtwn = (spacingbtwn/(self.pixelsize))
-        self.edgecoords = edgecoord
-        self.numbercells = ncells
+        self.edgecoords = edgecoord # list of pixel coords of annotation
+        self.numbercells = ncells # desired number of injections
         self.speed = speed
         
     def __del__(self):
@@ -51,62 +52,96 @@ class trajectoryimplementor(QThread):
         try:
             trajectoryarray = []
             for i in range(1,int(self.numbercells)):
+                # Injection number. Hardcoded 200 injections, but just errs out with exception when the injections exceed trajectory limits (because of y stepsize)
                 self.statusnumber = i
                 self.motorfinished = False
                 print("Injection number = ")
                 print(i)
 
+                # Stop button presed
                 if self.stopsignal == True:
                     break
 
+                # If first injection
                 if i == 1:
+                    # current pos = pixel coordinates of the last clicked tip
                     x0 = self.currentposition[0]
                     y0 = self.currentposition[1]
                     arrayf = self.edgecoords[int(i*self.spacingbtwn)]
+                    # desired pos = pixel coordinates along annotated edge
                     xf = arrayf[0]
                     yf = arrayf[1] 
 
+                # On subsequent injections (not first)
                 if i  > 1:
+                    # desired pos = pixel coordinates of injected site with spacing from just injected site
                     arrayf = self.edgecoords[int(i*self.spacingbtwn)]
                     print(int(i*self.spacingbtwn))
                     xf = arrayf[0]
                     yf = arrayf[1]
+                    # current pos = pixel coordinates on the edge of the "just injected site"
                     array1 = self.edgecoords[(int((i-1)*self.spacingbtwn))]
                     x0 = array1[0]
                     y0 = array1[1]
 
+                # Compute the desired change in pixel coordinates of the micropipette tip
                 x = xf - x0
                 y = yf - y0
                 print('x, y = ' + str(x) +',' +str(y))
+                # Calibration matrix? Idk why Gabi hardcoded the 0.033...
+                # a =   [-1     -sin(thetax)]
+                #       [-0     -cos(thetax)]
+                #
+                # where thetax = is the angle the y axis makes w/ verical and x axis makes with horizontal
+                # But theta isn't signed properly or general to any orientation.
+                # This matrix is only specific to the manipulator coming perpendicular from the right side of FOV
                 a = np.array([[-np.cos(0.0338853299), -np.sin(self.xthetarad)], [-np.sin(0.0338853299), -np.cos(self.xthetarad)]])
+                a = np.array([[-np.cos(self.xthetarad), -np.sin(self.xthetarad)], [-np.sin(self.xthetarad), -np.cos(self.xthetarad)]])
+                # Desired change in pixel coordinates of the micropipette tip
                 b = np.array ((x,y))
                 print('a =' + str(a))
                 print('b = ' + str(b))
-
+                # Solve for the psuedo change in pixel coordiantes (as if the manipulator units were pixels) to move the micropiptte accordint to orientaiotn of maipulator axes
                 manipulator = np.linalg.solve(a, b)
                 print('dx,dy = ' + str(manipulator))
+                # Convert the psuedo chagne in pixel coordinates to actual change in manipulator coordinates
                 manipulator_motorscaled = manipulator*self.pixelsize*1000 #(in nanometers)
                 print('manipulator_motorscaled =' + str(manipulator_motorscaled))
-
+                # append change in manipualtor cooridnates to array
                 trajectoryarray.append([manipulator_motorscaled])
+                # Get the change in manipulator coordinates aka (k = manipulator_motorscaled). idk why gabi did this
                 k = trajectoryarray[(i-1)]
                 print('k = ' + str(k))
+                # z corrected is actual computing necessary change in d coordiante accorording to pipette anle (thetaz)
+                # cos(pipette angle) = x/d so x/cos(pipette angle) -> d
                 zcorrected = (k[0][0]/(np.cos(self.thetaz)))
                 print('zcorrected =' + str(zcorrected))
-                zcorrected = zcorrected
+                zcorrected = zcorrected # why?
+                # Resolve for x but negate it? because zcorrected was solved as d?
+                # cos(pipette angle) = x/d so -d*cos(pipette angle -> -x
                 zdrift = -zcorrected*(np.cos(self.thetaz))
+                print(zdrift)
 
-                self.motorcalib3 = mc(zdrift, zcorrected, k[0][1], self.approachdist, self.depthintissue, self.speed)
-                self.connect(self.motorcalib3, SIGNAL("finished()"),self.turnsignal)
+                # 4 axis trajectory
+                dx = manipulator_motorscaled[0]
+                dy = manipulator_motorscaled[1]
+                dz_per_dd = np.sin(np.abs(self.thetaz))
+                dx_per_dd = np.cos(np.abs(self.thetaz))
+                self.motorcalib3 = mci(i,dx,dy,dz_per_dd,self.approachdist, self.depthintissue, self.speed)
+                # 3 axis trajecotry
+                # self.motorcalib3 = mc(zdrift, zcorrected, k[0][1], self.approachdist, self.depthintissue, self.speed)
+                # Run trajecotory
+                self.motorcalib3.finished.connect(self.turnsignal) # when injects positoin, sends signal which will cause next injection positoin
                 self.motorcalib3.start()
 
+                # Keep waiting until injected, but then move to next position when current position injected
                 while self.motorfinished == False:
                     time.sleep(0.1)
                     
             print(trajectoryarray)
 
         except:
-
+            print(traceback.format_exc())
             print('except')
             print(trajectoryarray)
 
